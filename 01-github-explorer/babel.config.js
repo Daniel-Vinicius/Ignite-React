8011fe9e58964ce1208dffbaaf9b1dd6d905f74b
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-03-09 10:02:20 -0300

    Typescript sendo migrado

--- 01-github-explorer/babel.config.js
+++ 01-github-explorer/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
+    '@babel/preset-typescript',
     ['@babel/preset-react', {
       // Pra não precisar importar o react em todos os arquivos .jsx/.tsx
       runtime: 'automatic'
