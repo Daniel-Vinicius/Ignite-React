40138109709cdfe7dc32e28cad04a7b840c2fedb
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-03-17 21:11:57 -0300

    Parte 1

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global.ts/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>DT Money</h1>
+      <GlobalStyle />
     </div>
   );
 }
