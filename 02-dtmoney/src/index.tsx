617e5ee3bc760a6ddc22bbaad1683b82463b0dc5
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-03-18 19:17:43 -0300

    Parte 3 MirageJS

--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -1,6 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transitions", () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de site",
+          amount: 12000,
+          type: "deposit",
+          category: "Venda",
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: "Computador",
+          amount: 6000,
+          type: "withdraw",
+          category: "Compra",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
