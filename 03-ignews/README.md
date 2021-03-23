a0595cb9736f9c6cd38d7876f6abfe2d880e4faa
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-03-23 10:45:48 -0300

    API Routes no Next.jS

---
+++ 03-ignews/README.md
@@ -0,0 +1,11 @@
+# Ignews
+
+### Acessar
+
+- https://nextjs.org/docs/authentication
+
+Para autenticar com Google
+
+- https://next-auth.js.org/providers/google
+- https://developers.google.com/identity/protocols/oauth2
+- https://console.cloud.google.com/projectselector2/apis/credentials
--- 03-ignews/src/components/SignInButton/index.tsx
+++ 03-ignews/src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import React from "react";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Daniel Vinícius
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign With Github
     </button>
