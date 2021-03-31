b9cc79f941e3ca125c32385b1357fab3c4095c4c
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-03-31 19:51:36 -0300

    Configurando Projeto

---
+++ 04-dashgo/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DashGo</title>
+      </Head>
+      <h1>DashGo</h1>
+    </div>
+  );
+}
