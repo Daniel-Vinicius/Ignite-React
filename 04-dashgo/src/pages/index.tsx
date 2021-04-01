1c91ff5d9e25efe383a2582d093dcc2f4ad81cce
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-04-01 16:16:19 -0300

    Login e Dashboard

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,12 +1,39 @@
 import Head from "next/head";
+import { Flex, Button, Stack } from "@chakra-ui/react";
 
-export default function Home() {
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <div>
+    <>
       <Head>
         <title>DashGo</title>
       </Head>
-      <h1>DashGo</h1>
-    </div>
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Flex
+          as="form"
+          w="100%"
+          maxWidth="22.5rem"
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+        >
+          <Stack spacing="4">
+            <Input name="email" type="email" label="E-mail" isRequired={true} />
+            <Input
+              name="password"
+              type="password"
+              label="Senha"
+              isRequired={true}
+            />
+          </Stack>
+
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Flex>
+      </Flex>
+    </>
   );
 }
