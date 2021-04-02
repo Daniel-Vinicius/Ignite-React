fb819f5fcf8f21bb7c6b6f626526ebdecfeec471
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-04-02 19:54:24 -0300

    Validação de campos com Chakra + React Hook Form

--- 04-dashgo/src/components/Form/Input.tsx
+++ 04-dashgo/src/components/Form/Input.tsx
@@ -1,18 +1,26 @@
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
+
 import {
   Input as ChakraInput,
   FormLabel,
   FormControl,
   InputProps as ChakraInputProps,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps): JSX.Element {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+): JSX.Element => {
   return (
-    <FormControl>
+    <FormControl isInvalid={Boolean(error)}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
 
       <ChakraInput
@@ -25,8 +33,13 @@ export function Input({ name, label, ...rest }: InputProps): JSX.Element {
           bgColor: "gray.900",
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
+
+      {Boolean(error) && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,9 +1,23 @@
 import Head from "next/head";
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import { Flex, Button, Stack } from "@chakra-ui/react";
 
 import { Input } from "../components/Form/Input";
 
+type SignInFormData = { email: string; password: string };
+
 export default function SignIn() {
+  const { register, handleSubmit, formState, errors } = useForm();
+
+  console.log(errors);
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(values);
+  };
+
   return (
     <>
       <Head>
@@ -18,18 +32,31 @@ export default function SignIn() {
           p="8"
           borderRadius={8}
           flexDir="column"
+          onSubmit={handleSubmit(handleSignIn)}
         >
           <Stack spacing="4">
-            <Input name="email" type="email" label="E-mail" isRequired={true} />
+            <Input
+              name="email"
+              label="E-mail"
+              type="email"
+              ref={register({ required: "E-mail é obrigatório" })}
+              error={errors.email}
+            />
             <Input
               name="password"
-              type="password"
               label="Senha"
-              isRequired={true}
+              type="password"
+              ref={register}
             />
           </Stack>
 
-          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+            isLoading={formState.isSubmitting}
+          >
             Entrar
           </Button>
         </Flex>
