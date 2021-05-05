dd0bb3611151d1ad136d037323226292b2c14d86
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-05-05 19:49:34 -0300

    Validando visitante

--- 05-nextauth/pages/index.tsx
+++ 05-nextauth/pages/index.tsx
@@ -5,6 +5,7 @@ import { parseCookies } from "nookies";
 import { useAuth } from "../contexts/AuthContext";
 
 import styles from "../styles/Home.module.css";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -40,19 +41,8 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies["nextauth.token"]) {
-    return {
-      redirect: {
-        destination: "/dashboard",
-        permanent: false,
-      },
-    };
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {},
   };
-};
+});
