dd30feb8806c3f7517822455acd3e3c9c5ecd8d8
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-05-10 18:44:35 -0300

    bundle analyzer

--- 06-performance/components/ProductItem.tsx
+++ 06-performance/components/ProductItem.tsx
@@ -1,6 +1,8 @@
 import { memo, useState } from "react";
 import dynamic from "next/dynamic";
 
+import lodash from "lodash";
+
 import { AddProductToWishlistProps } from "./AddProductToWishlist";
 
 const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
@@ -54,6 +56,6 @@ function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
 export const ProductItem = memo(
   ProductItemComponent,
   (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
+    return lodash.isEqual(prevProps.product, nextProps.product);
   }
 );
