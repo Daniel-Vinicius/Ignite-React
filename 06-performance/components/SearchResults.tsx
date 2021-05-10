ea7354d72eb452c124df5604a8b53610d8279bb6
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-05-10 13:07:03 -0300

    useMemo

--- 06-performance/components/SearchResults.tsx
+++ 06-performance/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -9,8 +11,22 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((acc, product) => {
+      // const inverseSqrt5 = 0.44721359549995793928183473374626;
+      // const phi = 1.6180339887498948482045868343656;
+
+      // const calculopesado = Math.floor(
+      //   Math.pow(phi, product.price) * inverseSqrt5 + 0.5
+      // );
+
+      return acc + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map((product) => {
         return <ProductItem product={product} />;
       })}
