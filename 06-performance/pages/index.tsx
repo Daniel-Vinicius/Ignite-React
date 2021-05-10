da3d2a7e2ffc8fc139bf690d95d490b1d2209329
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-05-10 12:14:18 -0300

    Exemplo de aplicação

--- 06-performance/pages/index.tsx
+++ 06-performance/pages/index.tsx
@@ -1,5 +1,39 @@
-const IndexPage = () => (
-    <h1>Hello Next.js 👋</h1>
-)
+import { FormEvent, useState } from "react";
 
-export default IndexPage
+import { SearchResults } from "../components/SearchResults";
+
+const Home = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
+  );
+};
+
+export default Home;
