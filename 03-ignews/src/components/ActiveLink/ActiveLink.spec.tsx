68917949a056e182b816a264c7d2738df5e3905b
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-05-19 14:03:21 -0300

    Testando primeiro componente

---
+++ 03-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,36 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+describe("ActiveLink component", () => {
+  it("renders correctly", () => {
+    const { debug, getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    // debug(); // mostra no console um html, quando se roda os tests `yarn test`
+
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  test("active link is receiving active class", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText("Home")).toHaveClass("active");
+  });
+});
